8455550b4a44f3447071d7260e635f3cc9e02988
Hoang Van Thanh <thanhthanh125521@gmail.com>
2022-04-06 20:04:00 +0700

    update

--- src/app/controllers/SiteControllers.js
+++ src/app/controllers/SiteControllers.js
@@ -2,13 +2,14 @@ const  Course = require('./../../models/Course')
 class SiteController {
   //GET - home
   index(req,res){
-    Course.find({},function(err,docs){
-      if(!err){
-         res.json(docs);
-         return ;
-      }
-      res.status(400).json({err:'ERROR'})
-    })
+    // Course.find({},function(err,docs){
+    //   if(!err){
+    //      res.json(docs);
+    //      return ;
+    //   }
+    //   res.status(400).json({err:'ERROR'})
+    // })
+    res.render('home')
   }
   //GET = /search
   search(req,res){
