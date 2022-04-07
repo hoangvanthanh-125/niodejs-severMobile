b8c6b5e91fc0c28b98a4b76be96bb489f83a653a
Hoang Van Thanh <thanhthanh125521@gmail.com>
2022-04-07 10:43:59 +0700

    t

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const URI = "mongodb+srv://thanh125:yeulol21@mobile.uuom0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 async function connect (){
   try {
-    await mongoose.connect('mongodb://localhost:27017/education');
+    await mongoose.connect(URI);
     console.log('connect succsessfully!!');
     
   } catch (error) {
