69f1e3a220b71ac15bfc1037aa97c566c348448e
Hoang Van Thanh <thanhthanh125521@gmail.com>
2022-04-10 10:02:19 +0700

    filter price

--- src/app/controllers/ProductsController.js
+++ src/app/controllers/ProductsController.js
@@ -1,9 +1,20 @@
-const {ProductModel} = require("./../../models/Products");
+const { ProductModel } = require("./../../models/Products");
 class ProductsController {
   //GET /
   showAllproduct = async (req, res) => {
+    const { min, max } = req.query;
+    if ((min && !max) || (!min && max)) {
+      return res.status(400).json({ message: "Thiếu min or max" });
+    }
+    const query = {};
+    if (min && max) {
+      if (typeof Number(min) !== "number" || typeof Number(max) !== "number") {
+        return res.status(400).json({ message: "min and max phai la 1 so" });
+      }
+      query.price = { $gt: min, $lt: max };
+    }
     try {
-      const products = await ProductModel.find();
+      const products = await ProductModel.find(query);
       res.status(200).json(products);
     } catch (error) {
       res.status(500).json({
@@ -12,34 +23,32 @@ class ProductsController {
     }
   };
   //Get by id : /products/:id
-  showProductById = async  (req, res) =>  {
+  showProductById = async (req, res) => {
     try {
       const _id = req.params.id;
       const product = await ProductModel.findById(_id);
       res.status(200).json(product);
-      
     } catch (error) {
-      res.status(500).json({error})
+      res.status(500).json({ error });
     }
-
-  }
-  //get product by category 
+  };
+  //get product by category
   //product/category/:id
-   
+
   //put : /products/:id
   updateProduct(req, res) {}
 
-  createProduct = async(req, res) => {
-    let listImages =  req.files.map(file => {
+  createProduct = async (req, res) => {
+    let listImages = req.files.map((file) => {
       const index = file.path.indexOf("public");
       return file.path.slice(index + 6);
-    })
-  if(!req.files){
-    return res.status(400).json({message:"Upload file failed"});
-  }
+    });
+    if (!req.files) {
+      return res.status(400).json({ message: "Upload file failed" });
+    }
     try {
       const data = req.body;
-      const newProduct = new ProductModel({...data,images:listImages});
+      const newProduct = new ProductModel({ ...data, images: listImages });
       const product = await newProduct.save();
       res.status(200).json(product);
     } catch (error) {
@@ -47,6 +56,5 @@ class ProductsController {
     }
   };
 }
-
 
 module.exports = new ProductsController();
