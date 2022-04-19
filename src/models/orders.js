d64687254f6c1ef49dc0fc425849e8ef4db22347
hoangvanthanh-521125 <thanhthanh125521@gmail.com>
2022-04-20 00:23:38 +0700

    update order

--- src/models/orders.js
+++ src/models/orders.js
@@ -6,6 +6,11 @@ const {CartSchema} = require('./../models/cart')
 const Orders = new Schema({
   product:{type:CartSchema,required:true},
   userId:{type:Schema.Types.Mixed,required:true},
+  userName:{type:String,required:true},
+  address:{type:String,required:true},
+  phoneNumber:{type:String,required:true},
+  orderDate:{type:Date,default:+new Date()},
+  shippedDate:{type:Date,default:+new Date() + 3*24*60*60*1000},
   status:{
     type:String,
     enum : ['delivering','cancelled','received'],
